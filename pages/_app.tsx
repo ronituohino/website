0cebef60f48c5cf7c1719f08d3cad1143c2078f4
Roni Tuohino <tuohinoroni@gmail.com>
2023-01-21 00:57:34 +0200

    Figuring out styling, synthwave is pretty cool

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,10 @@ import "../styles/globals.css";
 import Head from "next/head";
 import type { AppProps } from "next/app";
 
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+import Link from "next/link";
+
 import { Roboto } from "@next/font/google";
 const roboto = Roboto({ weight: "400" });
 
@@ -13,9 +17,26 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="description" content="Roni Tuohino portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <main className={roboto.className}>
-        <Component {...pageProps} />
-      </main>
+
+      <div className={roboto.className}>
+        <header>
+          <Link href="/">Index</Link>
+          <Link href="/blog">Blog</Link>
+
+          <Link target="_blank" href="https://github.com/ronituohino">
+            <BsGithub />
+          </Link>
+          <Link target="_blank" href="https://www.linkedin.com/in/ronituohino/">
+            <BsLinkedin />
+          </Link>
+          <Link href="mailto:tuohinoroni@gmail.com">
+            <MdEmail />
+          </Link>
+        </header>
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </div>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,25 @@
-import { BsGithub, BsLinkedin } from "react-icons/bs";
-import { MdEmail } from "react-icons/md";
+import Link from "next/link";
 
 import { Text } from "../components/Text";
-
-import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <header>
-        <Link href="/">Index</Link>
-        <Link href="/blog">Blog</Link>
-
-        <Link target="_blank" href="https://github.com/ronituohino">
-          <BsGithub />
-        </Link>
-        <Link target="_blank" href="https://www.linkedin.com/in/ronituohino/">
-          <BsLinkedin />
-        </Link>
-        <Link href="mailto:tuohinoroni@gmail.com">
-          <MdEmail />
-        </Link>
-      </header>
-
-      <article>
+      <section>
         <h1>Roni Tuohino</h1>
         <Link target="_blank" href="/sample.jpg">
           CV
         </Link>
-      </article>
+      </section>
 
-      <article>
+      <section>
         <h2>Projects</h2>
         <Text>Cool text!</Text>
-      </article>
+      </section>
 
-      <article>
+      <section>
         <h2>Contact</h2>
-      </article>
+      </section>
     </>
   );
 }
