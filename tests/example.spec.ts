56b9d26012ffb55bfe7cb8ef42394cb56cb29b05
Roni Tuohino <tuohinoroni@gmail.com>
2023-02-04 13:37:24 +0200

    Fix tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,18 +6,9 @@ test("index page works", async ({ page }) => {
   // The new url should be "/" (baseURL is used there)
   await expect(page).toHaveURL("/");
   // The new page should contain a p with ""
-  await expect(page.locator("p").first()).toContainText("Normal font");
   await expect(page).toHaveTitle("Roni Tuohino");
 
-  await expect(page.locator("p").first()).not.toContainText("Cool font trick");
-});
-
-test("ssr works", async ({ page }) => {
-  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto("/ssr");
-  // The new url should be "/" (baseURL is used there)
-  await expect(page).toHaveURL("/ssr");
-  // The new page should contain a p with ""
-  await expect(page.locator("p").first()).toContainText("SSR");
-  await expect(page).toHaveTitle("Roni Tuohino");
+  await expect(page.locator("h1").first()).toContainText(
+    "Website under construction"
+  );
 });
