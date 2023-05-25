c6c037fa5d3dde88c3d95f7dbfd7f712e2a12425
Roni Tuohino <tuohinoroni@gmail.com>
2023-05-25 13:27:35 +0200

    Fix right background deco stretch

--- app/projects/gparticles/page.tsx
+++ app/projects/gparticles/page.tsx
@@ -14,59 +14,59 @@ export const metadata: Metadata = {
 
 export default function GParticles() {
   return (
-    <>
-      <div className={styles.background}>
-        <article className={styles.container}>
-          <header className={styles.header}>
-            <h1 className={styles.title}>GParticles</h1>
+    <div className={styles.background}>
+      <div className={styles.backgroundLeft} />
+      <div className={styles.backgroundRight} />
+      <article className={styles.container}>
+        <header className={styles.header}>
+          <h1 className={styles.title}>GParticles</h1>
 
-            <ul className={styles.links}>
-              <li>
-                <LinkIcon
-                  label="Project GitHub"
-                  href="https://github.com/ronituohino/GParticles"
-                  icon={<GitHub title="" />}
-                  className={styles.githubLink}
-                />
-              </li>
-              <li>
-                <LinkIcon
-                  label="Project on Google Play"
-                  href="https://play.google.com/store/apps/details?id=com.Tuohino.GParticles"
-                  icon={<GooglePlay title="" />}
-                  className={styles.googleLink}
-                />
-              </li>
-            </ul>
+          <ul className={styles.links}>
+            <li>
+              <LinkIcon
+                label="Project GitHub"
+                href="https://github.com/ronituohino/GParticles"
+                icon={<GitHub title="" />}
+                className={styles.githubLink}
+              />
+            </li>
+            <li>
+              <LinkIcon
+                label="Project on Google Play"
+                href="https://play.google.com/store/apps/details?id=com.Tuohino.GParticles"
+                icon={<GooglePlay title="" />}
+                className={styles.googleLink}
+              />
+            </li>
+          </ul>
 
-            <p>
-              A really quick Unity project. The main idea was to achieve some
-              cool effects with particles and physics. For some reason this kind
-              of took off in the Play Store. I tried to optimize it by moving
-              all the calculations to the GPU but I had some other stuff come up
-              while trying to learn compute shaders. I guess now I could just
-              ask ChatGPT to write the shader lol.
-            </p>
-          </header>
+          <p>
+            A really quick Unity project. The main idea was to achieve some cool
+            effects with particles and physics. For some reason this kind of
+            took off in the Play Store. I tried to optimize it by moving all the
+            calculations to the GPU but I had some other stuff come up while
+            trying to learn compute shaders. I guess now I could just ask
+            ChatGPT to write the shader lol.
+          </p>
+        </header>
 
-          <div className={styles.pics}>
-            <Picture
-              title="Purple particles circulating around multiple points the user is pressing on."
-              width="300"
-              imgWidth="110"
-              frame="phone1"
-              src="/pictures/projects/gparticles/pinkishpurple.jpg"
-            />
-            <Picture
-              title="Yellow and green particles shooting off into different directions."
-              width="300"
-              imgWidth="110"
-              frame="phone2"
-              src="/pictures/projects/gparticles/yellowishgreen.jpg"
-            />
-          </div>
-        </article>
-      </div>
-    </>
+        <div className={styles.pics}>
+          <Picture
+            title="Purple particles circulating around multiple points the user is pressing on."
+            width="300"
+            imgWidth="110"
+            frame="phone1"
+            src="/pictures/projects/gparticles/pinkishpurple.jpg"
+          />
+          <Picture
+            title="Yellow and green particles shooting off into different directions."
+            width="300"
+            imgWidth="110"
+            frame="phone2"
+            src="/pictures/projects/gparticles/yellowishgreen.jpg"
+          />
+        </div>
+      </article>
+    </div>
   );
 }
