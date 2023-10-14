6f12f2b32e4b5964e2bbf1fec129d49452a5e9d2
Roni Tuohino <tuohinoroni@gmail.com>
2023-10-14 21:48:15 +0300

    Remove useless stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import solidJs from "@astrojs/solid-js";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ronituohino.github.io", // Update this once custom domain set
-  base: "/website", // Remove this once custom domain set
   // Also read this https://docs.astro.build/en/guides/deploy/github/
   integrations: [solidJs(), sitemap()],
   // Enable passing class names from parent to child components
