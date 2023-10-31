98d2c472808d2c9844fac4326ca545a2978b4018
Roni Tuohino <tuohinoroni@gmail.com>
2023-10-31 22:18:26 +0200

    Start applying figma design to page

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -1,5 +1,8 @@
 import { createSignal } from "solid-js";
 import type { JSX } from "solid-js";
+import { BsQuestion } from "solid-icons/bs";
+
+import styles from "./Description.module.css";
 
 type DescriptionProps = {
   first?: JSX.Element;
@@ -12,15 +15,16 @@ export function Description({ first, second, third }: DescriptionProps) {
   const elements = [first, second, third];
 
   return (
-    <>
+    <div class={styles.description}>
       {elements[selected()]}
       <button
         onClick={() => {
           setSelected(selected() + 1);
         }}
+        class={styles.button}
       >
-        increase by one
+        <BsQuestion class={styles.icon} />
       </button>
-    </>
+    </div>
   );
 }
